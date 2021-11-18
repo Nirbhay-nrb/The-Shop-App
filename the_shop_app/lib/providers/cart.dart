05fd98d0f944f940a7049ef774197affad52b3ac
Nirbhay Makhija <nmakhija_be20@thapar.edu>
2021-11-18 20:54:52 +0530

    create cart.dart

---
+++ the_shop_app/lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    @required this.id,
+    @required this.price,
+    @required this.quantity,
+    @required this.title,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (existingValue) => CartItem(
+          id: existingValue.id,
+          price: existingValue.price,
+          quantity: existingValue.quantity + 1,
+          title: existingValue.title,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          price: price,
+          title: title,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
