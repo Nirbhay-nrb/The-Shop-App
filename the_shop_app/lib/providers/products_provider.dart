7bf3ff97bc6d47096a257f4ec847874cccfacaf9
Nirbhay <nmakhija_be20@thapar.edu>
2022-06-14 23:35:59 +0530

    deleting products

--- the_shop_app/lib/providers/products_provider.dart
+++ the_shop_app/lib/providers/products_provider.dart
@@ -68,4 +68,9 @@ class Products with ChangeNotifier {
     _items[prodIndex] = newProduct;
     notifyListeners();
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((element) => id == element.id);
+    notifyListeners();
+  }
 }
--- the_shop_app/lib/widgets/user_product_item.dart
+++ the_shop_app/lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:the_shop_app/providers/products_provider.dart';
 import 'package:the_shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -30,7 +32,9 @@ class UserProductItem extends StatelessWidget {
             icon: Icon(
               Icons.delete,
             ),
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            },
             color: Theme.of(context).errorColor,
           ),
         ],
