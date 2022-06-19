7a59036e376579aed48bbbebd139c856c093b3be
Nirbhay <nmakhija_be20@thapar.edu>
2022-06-20 00:11:23 +0530

    using FutureBuilder

--- the_shop_app/lib/screens/orders_screen.dart
+++ the_shop_app/lib/screens/orders_screen.dart
@@ -12,48 +12,66 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isInit = true;
-  var _isLoading = false;
+  // var _isInit = true;
+  // var _isLoading = false;
 
   Future<void> _refreshOrders(BuildContext context) async {
     await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
   }
-
-  @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      setState(() {
-        _isLoading = true;
-      });
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-    _isInit = false;
-    super.didChangeDependencies();
-  }
+  // you can use this in a stateful widget .. but if u want a statelesswidget
+  // then use FutureBuilder (more elegant way doing the same thing)
+  // @override
+  // void didChangeDependencies() {
+  //   if (_isInit) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     });
+  //   }
+  //   _isInit = false;
+  //   super.didChangeDependencies();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: () => _refreshOrders(context),
-              child: ListView.builder(
-                itemCount: orderData.orders.length,
-                itemBuilder: (ctx, i) => OrderItem(
-                  orderData.orders[i],
-                ),
-              ),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            // we are done fetching the data
+            if (dataSnapShot.error != null) {
+              // error has occured
+              return Center(
+                child: Text('Error occured'),
+              );
+            } else {
+              return Consumer<Orders>(builder: (ctx, orderData, child) {
+                return RefreshIndicator(
+                  onRefresh: () => _refreshOrders(context),
+                  child: ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => OrderItem(
+                      orderData.orders[i],
+                    ),
+                  ),
+                );
+              });
+            }
+          }
+        },
+      ),
     );
   }
 }
