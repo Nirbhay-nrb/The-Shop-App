cbb7c585644ecae1df0119e37ca2dd5797c94d47
Nirbhay <nmakhija_be20@thapar.edu>
2022-07-04 21:09:48 +0530

    adding Animated Container to Orders

--- the_shop_app/lib/widgets/order_item.dart
+++ the_shop_app/lib/widgets/order_item.dart
@@ -14,28 +14,37 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 150, 200) : 95,
+      // in case the card is not expanded, then we need a minimum height
+      // we can play with any height and put something we like
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.only(left: 15, right: 15, top: 4, bottom: 5),
-              height: min(widget.order.products.length * 20.0 + 20, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 20, 100)
+                  : 0,
+              // if not expanded then this container will not be shown
               child: ListView.builder(
                 itemCount: widget.order.products.length,
                 itemBuilder: (ctx, i) => Row(
@@ -56,7 +65,8 @@ class _OrderItemState extends State<OrderItem> {
                 ),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
